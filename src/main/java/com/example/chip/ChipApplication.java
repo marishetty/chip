24245a752d9ae154a2cb9bf6964fbe9a6a42a000
marishetty <swetha.marishetty@gmail.com>
2019-06-09 19:12:55 +0530

    This is basic setup of hello world in spring boot

---
+++ src/main/java/com/example/chip/ChipApplication.java
@@ -0,0 +1,14 @@
+package com.example.chip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipApplication.class, args);
+		System.out.println("This is welocme spring boot application hello world");
+	}
+
+}
